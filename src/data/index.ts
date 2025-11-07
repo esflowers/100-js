3f8aa0dfab968551ea3048f4fb73746d195b4409
esflowers <estrada.flsw@gmail.com>
2025-11-06 18:24:22 -0600

    edit index file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,5 @@
 import type { Link } from "../types";
 
-export const dev: Link = {
-    title: 'esflowers',
-    url: 'https://esflowers.icu/'
-}
-
 export const name = '100 JS'
 export const title = '100 proyectos de JavaScript'
 export const description = ''
@@ -18,23 +13,21 @@ export const site: Link = {
 
 export const ig: Link = {
     title: 'Instagram',
-    url: 'https://www.instagram.com/esflowers._/',
-    label: 'instagram.com/esflowers._'
+    url: 'https://instagram.com/esflowers._/'
 }
 
 export const repository: Link = {
     title: 'GitHub',
-    url: 'https://github.com/esflowers/100-js'
+    url: 'https://github.com/esflowers/100-js',
+    label: 'Repositorio'
 }
 
 export const midudev: Link = {
     title: 'Midudev',
-    url: 'https://midu.dev/',
-    label: 'midu.dev'
+    url: 'https://midu.dev/'
 }
 
 export const creator: Link = {
     title: '100 proyectos JS',
-    url: 'https://www.javascript100.dev/',
-    label: 'javascript100.dev'
+    url: 'https://www.javascript100.dev/'
 }
